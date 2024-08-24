98b4a74a62beb108d47229482f599f1c7b4842aa
AnsharaArshad <ansharaarshad660@gmail.com>
2024-08-24 13:01:20 -0700

    Add files via upload

---
+++ chp 03 variables for numbers/script.js
@@ -0,0 +1,33 @@
+// Qno01:
+// var age1 = 18;
+// var age2 = " years old";
+// var both = age1 + age2;
+// alert(both);
+
+// Qno02:
+// var num = 1;
+// alert("You have visited a side " +num+ " times");
+//  num = num + 1;
+// alert("You have visited a side " +num+ " times");
+//  num = num + 1;
+// alert("You have visited a side " +num+ " times");
+//  num = num + 1;
+// alert("You have visited a side " +num+ " times");
+
+// Qno03:
+// var birthYear = 2006;
+// var year = "My birth year is 2006.. <br/> Data type of my declared variable is number..";
+// document.write(year);
+
+// Qno04:
+// var storemail = "www.xyzClothing.com";
+// var name = "Goerge";
+// var productTitle = "sweatshirt";
+// var quantity = 2;
+// var statement = "Goerge ordered 2 sweatshirts on xyzClothingStore..";
+// document.write(statement);
+
+
+
+
+
