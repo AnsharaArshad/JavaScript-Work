bb4957b5545a5ae3c509305c9893ac4bf17063be
AnsharaArshad <ansharaarshad660@gmail.com>
2024-08-24 12:48:58 -0700

    Add files via upload

---
+++ chp 02 variables for string/script.js
@@ -0,0 +1,43 @@
+// Qno01:
+// var name = "username";
+// alert(name);
+
+// Qno02:
+// var myName = "AnsharaArshad";
+// alert(myName);
+
+// Qno03:
+// var message = "Hello World";
+// alert(message);
+// alert("Hello World");
+
+// Qno04:
+// var studentData = "Jhone Doe";
+// alert(studentData);
+// var studentData = "15 Years Old";
+// alert(studentData);
+// var studentData = "Certified Mobile Application Development";
+// alert(studentData);
+
+// Qno05:
+// var food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+// alert(food);
+
+// Qno06:
+// var email1 = "ansharaarshad660@gmail.com...";
+// var email2 = " My Email Address Is ansharaarshad660@gmail.com";
+// var concatenate = email1 + email2;
+// alert(concatenate);
+
+// Qno07:
+// var book = "A smarter way to learn JavaScript";
+// alert("Im trying to learn from the book A smarter way to learn JavaScript..");
+
+// Qno08:
+// var heading = "Yahh! I can write HTML content through JavaScript..";
+// document.write(heading);
+
+// Qno09:
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
---
+++ script.js
@@ -0,0 +1,25 @@
+// Qno01:
+// alert("Heyy, Welcome to this site!");
+
+// Qno02:
+// alert("Error! Please enter valid Password.");
+
+// Qno03:
+// alert("Welcome to JS land..\nHappy Coding!");
+
+// Qno04:
+// alert("Welcome to JS landing..");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+
+// Qno05:
+// alert("Hello!! i can run JS through my web browser's console");
+
+// Qno06:
+// function show(){
+//     var alert= Document.getElementById("container");
+//     alert.style.display="block"
+// }
+// function hide(){
+//     var alert=Document.getElementById("container");
+//     alert.style.display="none"
+// }
